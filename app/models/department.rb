65818181cd733b3545e7ec4b7677e1020c5e4584
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-04-04 13:17:03 +0300

    added turbo to department create

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,2 +1,5 @@
-class Department < ApplicationRecord
+# frozen_string_literal: true
+
+class Department < ApplicationRecord # rubocop:disable Style/Documentation
+  has_many :events, dependent: :destroy
 end
