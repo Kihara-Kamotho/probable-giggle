24eaef29593899371d308d969deece9bf89203af
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-04-04 15:11:01 +0300

    added event views

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :departments
+  resources :departments, shallow: true do
+    resources :events
+  end
+
   root 'departments#index'
 end
